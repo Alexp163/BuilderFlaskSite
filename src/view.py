905ca31599d80d1180b696f4e29dbeb1bee39778
Alexp163 <dr.p163@ya.ru>
2023-10-06 08:47:36 +0400

    Начало проекта. Создание основных файлов с подключением flask. Создание основных шаблонов страниц

---
+++ src/view.py
@@ -0,0 +1,23 @@
+from app import app
+from flask import render_template
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/case_costruite')
+def case_costruite():
+    """  Отображает страницу с построенными зданиями """
+    return render_template('/case_costruite.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('/login.html')
+
+
+@app.route('/registration')
+def registration():
+    return render_template('registration.html')
