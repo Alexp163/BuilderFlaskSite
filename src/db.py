46deefa6a3511dc20b2682176d965f42abf59cda
Alexp163 <dr.p163@ya.ru>
2023-10-19 22:30:49 +0400

    Подключение базы данных через db.py и models.py

--- src/db.py
+++ src/db.py
@@ -1,1 +1,6 @@
+from flask_sqlalchemy import SQLAlchemy
+from app import app
 
+
+app.config["SQLALCHEMY_DATABASE_URL"] = "sqlite: ///slite.db"
+db = SQLAlchemy(app)
--- src/models.py
+++ src/models.py
@@ -1,2 +1,27 @@
-if __name__ == '__main__':
-    main()
+from db import db
+
+# id - число
+# name_object - название объекта
+# type_object - тип объекта
+# material - материал изготовления
+# date_manufacture - дата изготовления
+# price - стоимость объекта
+# location - расположение объекта
+# ______________________________________
+# db.String(50) - текстовое поле заданной длины
+# db.Integer() - числовое поле
+# db.Column() - создание колонки(дополнительные поля nullable, unique, primary_key)
+# для id всегда задается primary_key=True
+
+class Object:
+    __tablename__ = "object"
+
+    id = db.Column(db.Integer(), primary_key=True) # идентификационный номер
+    name_object = db.Column(db.String()) # название объекта
+
+    type_object = db.Column(db.String(50)) # тип объекта
+    material = db.Column(db.String(50)) # материал изготовления
+    date_manufacture = db.Column(db.String(10)) # дата изготовления
+
+    price = db.Column(db.String(10)) # стоимость объекта
+    location = db.Column(db.String(50)) # расположение объекта
